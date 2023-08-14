34972be2593fdb340572e55a54d5b51fbc879c96
Kev <kevinhatran@utexas.edu>
2023-08-13 22:36:30 -0500

    first commit

---
+++ color-box/src/Box.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Box = ({
+    id,
+    handleRemove,
+    width = 5,
+    height = 5,
+    backgroundColor = "blue"
+}) => {
+    const remove = () => handleRemove(id);
+    return (
+        <div>
+            <div style={{
+                height: `${height}em`,
+                width: `${width}em`,
+                backgroundColor
+            }}>
+
+            </div>
+            <button onClick={remove}>Remove The Box!</button>
+        </div>
+    )
+}
+
+export default Box
